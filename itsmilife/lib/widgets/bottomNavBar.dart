7d6003f2bf19517b73e414b289cdd5b9fcc5dc51
thomasmrt13 <thomas3.martin@epitech.eu>
2023-08-27 17:54:29 +0200

    Update bottomNavBar.dart

--- itsmilife/lib/widgets/bottomNavBar.dart
+++ itsmilife/lib/widgets/bottomNavBar.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:itsmilife/pages/normal_user/activit%C3%A9s/activity.dart';
-import 'package:itsmilife/pages/normal_user/activités/activités.dart';
-import 'package:itsmilife/pages/normal_user/homepage/homepage.dart';
+// import 'package:itsmilife/pages/normal_user/activités/activités.dart';
+// import 'package:itsmilife/pages/normal_user/homepage/homepage.dart';
 import 'package:itsmilife/pages/common/settings/settings.dart';
 import 'package:flutter/cupertino.dart';
-import 'package:itsmilife/pages/professional/HomePro.dart';
+// import 'package:itsmilife/pages/professional/HomePro.dart';
 import 'package:itsmilife/pages/professional/Home_Pro.dart';
 import 'package:itsmilife/pages/professional/activity/categorypro.dart';
 import 'package:provider/provider.dart';
@@ -13,6 +13,7 @@ import 'package:itsmilife/pages/common/settings/darkModeProvider.dart';
 import '../pages/common/settings/RoleProvider.dart';
 import 'package:itsmilife/pages/normal_user/Home/screens/home_page.dart';
 
+// ignore: must_be_immutable
 class Home extends StatefulWidget {
   late int h_index;
 
